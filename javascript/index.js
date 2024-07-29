21b4b01ef96a37f9a6f85fd87ca92925d7f9bbdd
Maryam-coding <maryamnoor97@gmail.com>
2024-07-29 15:27:53 -0700

    changed the js

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,29 @@
 function updateTime() {
-let sanFranciscoElement = document.querySelector("#san-francisco");
-let sanFranciscoDateElement = sanFranciscoElement.querySelector(".date");
-let sanFranciscoTimeElement = sanFranciscoElement.querySelector(".time");
-let sanFranciscoTime = moment();
+    let sanFranciscoElement = document.querySelector("#san-francisco");
+    let sanFranciscoDateElement = sanFranciscoElement.querySelector(".date");
+    let sanFranciscoTimeElement = sanFranciscoElement.querySelector(".time");
+    let sanFranciscoTime = moment();
 
-sanFranciscoDateElement.innerHTML = sanFranciscoTime.format("MMMM Do YYYY");
-sanFranciscoTimeElement.innerHTML = sanFranciscoTime.format("h:mm:ss [<small>]A[</small>]");
-
-
-let londonElement = document.querySelector("#london");
-let londonDateElement = londonElement.querySelector(".date");
-let londonTimeElement = londonElement.querySelector(".time");
-let londonTime = moment();
-
-londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
-londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+    sanFranciscoDateElement.innerHTML = sanFranciscoTime.format("MMMM Do YYYY");
+    sanFranciscoTimeElement.innerHTML = sanFranciscoTime.format("h:mm:ss [<small>]A[</small>]");
 
 
-let lahoreElement = document.querySelector("#lahore");
-let lahoreDateElement = lahoreElement.querySelector(".date");
-let lahoreTimeElement = lahoreElement.querySelector(".time");
-let lahoreTime = moment();
+    let londonElement = document.querySelector("#london");
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment();
 
-lahoreDateElement.innerHTML = lahoreTime.format("MMMM Do YYYY");
-lahoreTimeElement.innerHTML = lahoreTime.format("h:mm:ss [<small>]A[</small>]");
+    londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+    londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+
+
+    let lahoreElement = document.querySelector("#lahore");
+    let lahoreDateElement = lahoreElement.querySelector(".date");
+    let lahoreTimeElement = lahoreElement.querySelector(".time");
+    let lahoreTime = moment();
+
+    lahoreDateElement.innerHTML = lahoreTime.format("MMMM Do YYYY");
+    lahoreTimeElement.innerHTML = lahoreTime.format("h:mm:ss [<small>]A[</small>]");
 
 }
 
