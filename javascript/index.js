3e595091e5a14fd5bfcf8e23c1d29a12034b5e51
Maryam-coding <maryamnoor97@gmail.com>
2024-07-30 11:18:21 -0700

    update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -37,7 +37,7 @@ function updateCity(event) {
     if (cityTimeZone === "current") {
         cityTimeZone = moment.tz.guess();
     }
-    let cityName = cityTimeZone.replace("_", " ").split("/"[1]);
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let cicitiesElement = document.querySelector("#cities");
     cicitiesElement.innerHTML = `
