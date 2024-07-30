2a06040e6893bfb5b2452eeb52d6efb7ce421d0f
Maryam-coding <maryamnoor97@gmail.com>
2024-07-29 17:17:18 -0700

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,35 @@
 function updateTime() {
+
+    // San Francisco
     let sanFranciscoElement = document.querySelector("#san-francisco");
+    if (sanFranciscoElement){
     let sanFranciscoDateElement = sanFranciscoElement.querySelector(".date");
     let sanFranciscoTimeElement = sanFranciscoElement.querySelector(".time");
     let sanFranciscoTime = moment();
 
     sanFranciscoDateElement.innerHTML = sanFranciscoTime.format("MMMM Do YYYY");
     sanFranciscoTimeElement.innerHTML = sanFranciscoTime.format("h:mm:ss [<small>]A[</small>]");
-
+    }
+    //London
     let londonElement = document.querySelector("#london");
+    if (londonElement) {
     let londonDateElement = londonElement.querySelector(".date");
     let londonTimeElement = londonElement.querySelector(".time");
     let londonTime = moment();
 
     londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
     londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
-
+    }
+    // Lahore
     let lahoreElement = document.querySelector("#lahore");
+    if (lahoreElement) {
     let lahoreDateElement = lahoreElement.querySelector(".date");
     let lahoreTimeElement = lahoreElement.querySelector(".time");
     let lahoreTime = moment();
 
     lahoreDateElement.innerHTML = lahoreTime.format("MMMM Do YYYY");
     lahoreTimeElement.innerHTML = lahoreTime.format("h:mm:ss [<small>]A[</small>]");
-
+    }
 }
 
 function updateCity(event) {
